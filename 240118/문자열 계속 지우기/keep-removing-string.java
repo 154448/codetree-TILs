0033e06c2eb244c4a20d40849ec43e931b821284
154448 <116735631+154448@users.noreply.github.com>
2024-01-18 18:18:35 +0900

    Title: 문자열 계속 지우기, Time: 217ms, Memory: 10MB, Status: Passed - CodeTree

--- 240118/문자열 계속 지우기/keep-removing-string.java
+++ 240118/문자열 계속 지우기/keep-removing-string.java
@@ -2,31 +2,42 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
         Scanner userIn = new Scanner(System.in);
-
+        //a - 전체 문자열
         String a = userIn.next();
+        //b - 부분 문자열
         String b = userIn.next();
-
+        //b 탐색 시, 사용하는 idx
         int idx = 0;
+        
+        //a 탐색시 사용하는 i
         int i = 0;
-
         while(i < a.length()){
+            //현재 위치의 글자
             char current = a.charAt(i);
-
+            //동일할 경우
             if(current == b.charAt(idx)){
                 idx += 1;
 
+                //완전히 일치하는 경우를 찾은 경우
                 if(idx == b.length()){
+                    //문자열을 자름
                     a = a.substring(0, i - b.length() + 1) + a.substring(i + 1);
+                    //위치 초기화
                     idx = 0;
+                    //문자열을 처음부터 다시 검색
                     i = -1;
                 }
             }
+            //다른 경우
             else{
-                idx = current == b.charAt(0) ? 1 : 0;
+                //idx를 초기화
+                i = i - idx;
+                idx = 0;
             }
+
             i += 1;
+            //System.out.printf("%d %d\n", i, idx);
         }
 
         System.out.printf("%s", a);
