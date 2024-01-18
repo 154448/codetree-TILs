fca32e7b3df5406d2dc9a3a148f1a27d4301a650
154448 <116735631+154448@users.noreply.github.com>
2024-01-18 15:54:12 +0900

    Title: 문자열 놀이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240118/문자열 놀이/play-with-string.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner userIn = new Scanner(System.in);
+
+        String s = userIn.next();
+        int n = userIn.nextInt();
+
+        for(int j = 0; j < n; j++){
+            int q = userIn.nextInt();
+
+            String newStr = "";
+
+            if(q == 1){
+                int first = userIn.nextInt() - 1;
+                int last = userIn.nextInt() - 1;
+
+                newStr += s.substring(0, first);
+                newStr += s.charAt(last);
+                newStr += s.substring(first + 1, last);
+                newStr += s.charAt(first);
+                newStr += s.charAt(last + 1);
+            }
+            else if(q == 2){
+                char key = userIn.next().charAt(0);
+                char value = userIn.next().charAt(0);
+
+                for(int i = 0; i < s.length(); i++){
+                    newStr += s.charAt(i) == key ? value : s.charAt(i);
+                }
+
+            }
+
+            System.out.printf("%s\n", newStr);
+
+            s = newStr;
+        }
+    }
+}
