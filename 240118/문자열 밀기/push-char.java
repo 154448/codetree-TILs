4e343c48f59038870cea44ce5dbdd6996ab0f39c
154448 <116735631+154448@users.noreply.github.com>
2024-01-18 18:24:27 +0900

    Title: 문자열 밀기, Time: 232ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240118/문자열 밀기/push-char.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner userIn = new Scanner(System.in);
+
+        String str = userIn.next();
+
+        System.out.printf("%s", str.substring(1) + str.charAt(0));
+    }
+}
