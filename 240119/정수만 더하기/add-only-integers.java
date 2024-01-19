7d27de0926a1bd1768e523b96090dff1109df209
154448 <116735631+154448@users.noreply.github.com>
2024-01-19 14:13:26 +0900

    Title: 정수만 더하기, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240119/정수만 더하기/add-only-integers.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner userIn = new Scanner(System.in);
+
+        String str = userIn.next();
+        int tot = 0;
+
+        for(int i = 0; i < str.length(); i++){
+            char c = str.charAt(i);
+
+            int n = (int)c;
+
+            if(n >= 48 && n <= 57){
+                tot += (n - 48);
+            }
+        }
+
+        System.out.printf("%d", tot);
+    }
+}
