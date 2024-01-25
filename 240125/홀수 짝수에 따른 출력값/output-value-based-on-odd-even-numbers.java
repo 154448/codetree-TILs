fe56b8558506857d6c22ca92dc7d76395bb9c411
154448 <116735631+154448@users.noreply.github.com>
2024-01-25 11:06:56 +0900

    Title: 홀수 짝수에 따른 출력값, Time: 148ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240125/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int sumNums(int n){
+        return n <= 2 ? 1 : n + sumNums(n - 2);
+    }
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner userIn = new Scanner(System.in);
+
+        int n = userIn.nextInt();
+
+        System.out.printf("%d", sumNums(n));
+    }
+}
