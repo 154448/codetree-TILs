87a1e596d889ab6919e75b183e0cd43cbbd172c2
154448 <116735631+154448@users.noreply.github.com>
2024-01-25 18:10:01 +0900

    Title: 중앙값 계산 2, Time: 133ms, Memory: 11MB, Status: Wrong Answer - CodeTree

---
+++ 240125/중앙값 계산 2/get-median-2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner userIn = new Scanner(System.in);
+
+        int n = userIn.nextInt();
+
+        int[] nums = new int[n];
+        Arrays.fill(nums, 100);
+
+        for(int i = 0; i < n; i++){
+            nums[i] = userIn.nextInt();
+            if(i % 2 == 0){
+                Arrays.sort(nums);
+                System.out.printf("%d ", nums[i / 2]);
+            }
+            
+
+        }
+    }
+}
