7f648337b55fcc4921ce00f83ff728fcfed4513b
154448 <116735631+154448@users.noreply.github.com>
2024-01-31 15:42:10 +0900

    Title: 연속되는 수 4, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240131/연속되는 수 4/continuous-number4.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner userIn = new Scanner(System.in);
+
+        int maxLength = 1;
+
+        int n = userIn.nextInt();
+        int[] nums = new int[n];
+
+        nums[0] = userIn.nextInt();
+        nums[1] = userIn.nextInt();
+
+        int cnt = 1;
+        // 0 : Increase , 1 : decrease
+
+        for(int i = 2; i < n; i++){
+            nums[i] = userIn.nextInt();
+
+            if(nums[i - 1] < nums[i]){
+                cnt += 1;
+                if(i == n - 1){
+                    maxLength = cnt > maxLength ? cnt : maxLength;
+                }
+            }
+            else{
+                maxLength = cnt > maxLength ? cnt : maxLength;
+                cnt = 1;
+            }
+        }
+
+        System.out.printf("%d", maxLength);
+    }
+}
