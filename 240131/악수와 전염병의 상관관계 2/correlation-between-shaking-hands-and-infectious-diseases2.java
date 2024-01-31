f749636b8dfa0acd55b8bf2168f2a61cc9049c9f
154448 <116735631+154448@users.noreply.github.com>
2024-01-31 18:55:11 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 178ms, Memory: 11MB, Status: Wrong Answer - CodeTree

---
+++ 240131/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+class Info implements Comparable<Info>{
+    int idx;
+    int x;
+    int y;
+
+    public Info(int idx, int x, int y){
+        this.idx = idx;
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y;
+    }
+
+    public int compareTo(Info other){
+        return this.idx - other.idx;
+    }
+
+    
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner userIn = new Scanner(System.in);
+
+        int n = userIn.nextInt();
+        int k = userIn.nextInt();
+        int p = userIn.nextInt();
+        int t = userIn.nextInt();
+
+        Info[] information = new Info[t];
+
+        int[] state = new int[n];
+        state[p] = 1;
+
+        for(int i = 0; i < t; i++){
+            int s = userIn.nextInt();
+            int x = userIn.nextInt() - 1;
+            int y = userIn.nextInt() - 1;
+
+            information[i] = new Info(s, x, y);
+        }
+
+        Arrays.sort(information);
+
+        int cnt = 1;
+
+        for(int i = 0; i < t; i++){
+            int x = information[i].getX();
+            int y = information[i].getY();
+
+
+            if(state[x] == 1 || state[y] == 1){
+                if(cnt > k){
+                    break;
+                }
+                else{
+                    cnt += 1;
+                }
+                state[x] = 1;
+                state[y] = 1;
+            }
+        }
+
+        for(int i = 0; i < n; i++){
+            System.out.printf("%d", state[i]);
+        }
+    }
+}
