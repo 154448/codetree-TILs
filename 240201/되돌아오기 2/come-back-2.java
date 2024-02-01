297445f7bfee7aadf09481e7acd0da1c41e85d62
154448 <116735631+154448@users.noreply.github.com>
2024-02-01 19:00:57 +0900

    Title: 되돌아오기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240201/되돌아오기 2/come-back-2.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner userIn = new Scanner(System.in);
+        
+        //N, E, S, W
+        int d = 0;
+        int[] dx = {0, 1, 0, -1};
+        int[] dy = {1, 0, -1, 0};
+
+        int x = 0;
+        int y = 0;
+        int t = 0;
+
+        int res = -1;
+
+        String order = userIn.nextLine();
+
+        for(int i = 0; i < order.length(); i++){
+            char direction = order.charAt(i);
+
+            if(direction == 'L' || direction == 'R'){
+                d = direction == 'L' ? (d - 1): (d + 1);
+                d %= 4;
+            }
+            else{
+                x += dx[d];
+                y += dy[d];
+            }
+            t += 1;
+
+            if(x == 0 && y == 0){
+                res = t;
+                break;
+            }
+        }
+        System.out.printf("%d", res);
+    }
+}
