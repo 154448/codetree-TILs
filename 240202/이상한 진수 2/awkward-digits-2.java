c31197b527ec5cfae270fd595df025f86c28222d
154448 <116735631+154448@users.noreply.github.com>
2024-02-02 16:22:22 +0900

    Title: 이상한 진수 2, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240202/이상한 진수 2/awkward-digits-2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner userIn = new Scanner(System.in);
+
+        String num = userIn.next();
+
+        int digit = num.length() - 1;
+        int tot = 0;
+
+        boolean isChange = false;
+
+        for(int i = 0; i < num.length(); i++){
+            int res = num.charAt(i) - '0';
+
+            if(!isChange && res == 0){
+                res = 1;
+                isChange = true;
+            }
+
+            tot += (Math.pow(2, digit) * res);
+            digit -= 1;
+        }
+
+        System.out.printf("%d", tot);
+    }
+}
