0e4580cc22c4031137530e49419c1631d9fd526c
154448 <116735631+154448@users.noreply.github.com>
2024-02-02 16:25:34 +0900

    Title: 이상한 진수 2, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

--- 240202/이상한 진수 2/awkward-digits-2.java
+++ 240202/이상한 진수 2/awkward-digits-2.java
@@ -9,11 +9,16 @@ public class Main {
 
         int digit = num.length() - 1;
         int tot = 0;
+        int cnt = 0;
 
         boolean isChange = false;
 
         for(int i = 0; i < num.length(); i++){
             int res = num.charAt(i) - '0';
+
+            if(res == 1){
+                cnt += 1;
+            }
 
             if(!isChange && res == 0){
                 res = 1;
@@ -22,6 +27,10 @@ public class Main {
 
             tot += (Math.pow(2, digit) * res);
             digit -= 1;
+        }
+
+        if(cnt == num.length()){
+            tot -= 1;
         }
 
         System.out.printf("%d", tot);
