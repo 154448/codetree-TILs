a9c3e0d16655e37efe0bbb7c9e1fc193b5f31f8d
154448 <116735631+154448@users.noreply.github.com>
2024-02-02 16:12:43 +0900

    Title: 체크판위에서 2, Time: 138ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240202/체크판위에서 2/on-the-checkboard-2.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner userIn = new Scanner(System.in);
+
+        int r = userIn.nextInt();
+        int c = userIn.nextInt();
+
+        char[][] block = new char[r][c];
+
+        for(int i = 0; i < r; i++){
+            for(int j = 0; j < c; j++){
+                block[i][j] = userIn.next().charAt(0);
+            }
+        }
+        char color = block[0][0];
+
+        int cnt = 0;
+
+        for(int i = 1; i < r; i++){
+            for(int j = 1; j < c; j++){
+                
+                //시작점과 다른색
+                if(block[i][j] != color){
+                    for(int x = i + 1; x < r; x++){
+                        for(int y = j + 1; y < c; y++){
+                            //시작점과 같은 색
+                            if(block[x][y] == color){
+                                for(int a = x + 1; a < r; a++){
+                                    for(int b = y + 1; b < c; b++){
+                                        if(block[a][b] != color){
+                                            cnt += 1;
+                                        }
+
+                                    }
+                                }
+                                
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.printf("%d", cnt);
+
+        
+    }
+}
