17dacda4ae846cb8d10ee0ccb5d6d19ddaaf8143
154448 <116735631+154448@users.noreply.github.com>
2024-02-02 13:48:05 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 523ms, Memory: 21MB, Status: Passed - CodeTree

--- 240202/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.java
+++ 240202/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.java
@@ -38,7 +38,7 @@ public class Main {
         }
 
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
+            for(int j = 0; j < m; j++){
                 System.out.printf("%d ", nums[i][j]);
             }
             System.out.println();
