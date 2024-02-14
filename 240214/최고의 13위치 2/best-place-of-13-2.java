593e050c2ff65c8ecf29249c50bedb91a58a2ea5
154448 <116735631+154448@users.noreply.github.com>
2024-02-14 17:34:04 +0900

    Title: 최고의 13위치 2, Time: 143ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240214/최고의 13위치 2/best-place-of-13-2.java
+++ 240214/최고의 13위치 2/best-place-of-13-2.java
@@ -17,18 +17,48 @@ public class Main {
         }
 
         int[] maxSum = new int[n];
+        int[] rowMax = new int[n];
 
         for(int r = 0; r < n; r++){
             int tmpM = 0;
+            
+            int tmpC = 0;
+
             for(int c = 0; c < n - 2; c++){
-                tmpM = Math.max(tmpM, plane[r][c] + plane[r][c + 1] + plane[r][c + 2]);
+                int tmp = plane[r][c] + plane[r][c + 1] + plane[r][c + 2];
+                if(tmp > tmpM){
+                    tmpM = tmp;
+                    tmpC = c;
+                }
             }
+
+            int secondM = 0;
+
+            for(int c = 0; c < n - 2; c++){
+                int tmp = 0;
+                for(int k = 0; k < 3; k++){
+                    int tC = c + k;
+                    
+                    if(tC >= tmpC || tC <= (tmpC + 2)){
+                        tmp = 0;
+                        break;
+                    }
+                    else{
+                        tmp += plane[r][c];
+                    }
+                }
+                secondM = Math.max(secondM, tmp);
+            }
+
             maxSum[r] = tmpM;
+            rowMax[r] = tmpM + secondM;
         }
 
+                                              
         Arrays.sort(maxSum);
+        Arrays.sort(rowMax);
 
-        System.out.printf("%d", maxSum[n - 1] + maxSum[n - 2]);
+        System.out.printf("%d", maxSum[n - 1] + maxSum[n - 2] > rowMax[n - 1] ? maxSum[n - 1] + maxSum[n - 2] : rowMax[n -1]);
 
 
     }
