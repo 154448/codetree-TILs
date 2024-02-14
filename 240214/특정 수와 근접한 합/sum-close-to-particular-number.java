46912225af565e771588df7d8efed22fa087e0eb
154448 <116735631+154448@users.noreply.github.com>
2024-02-14 18:45:46 +0900

    Title: 특정 수와 근접한 합, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240214/특정 수와 근접한 합/sum-close-to-particular-number.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner userIn = new Scanner(System.in);
+
+        int n = userIn.nextInt();
+        int s = userIn.nextInt();
+
+        int[] nums = new int[6];
+        int tot = 0;
+
+        for(int i = 0; i < n; i++){
+            int tmp = userIn.nextInt();
+            tot += tmp;
+            nums[i] = tmp;
+        }
+
+        int diff = s;
+        int[] target = new int[2];
+
+        for(int i = 0; i < n - 1; i++){
+            for(int j = i + 1; j < n; j++){
+                int tmp = tot - (nums[i] + nums[j]);
+
+                if(diff > Math.abs(s - tmp)){
+                    diff = Math.abs(s - tmp);
+                    target[0] = nums[i];
+                    target[1] = nums[j];
+                }
+            }
+        }
+
+        System.out.printf("%d", diff);
+    }
+}
