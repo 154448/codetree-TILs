7e510eacb0c215234ec182f38506d66407254ff3
154448 <116735631+154448@users.noreply.github.com>
2024-02-16 00:26:35 +0900

    Title: 한 가지로 열리는 자물쇠, Time: 152ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240216/한 가지로 열리는 자물쇠/one-way-lock.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner userIn = new Scanner(System.in);
+
+        int n = userIn.nextInt();
+
+        int[] num = new int[3];
+
+        for(int i = 0; i < 3; i++){
+            num[i] = userIn.nextInt();
+        }
+
+        int cnt = 0;
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                for(int k = 1; k <= n; k++){
+                    int[] tmp = {i, j, k};
+
+                    for(int idx = 0; idx < 3; idx++){
+                        int diff = tmp[idx] - num[idx];
+
+                        if(diff <= 2){
+                            cnt += 1;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.printf("%d", cnt);
+    }
+}
