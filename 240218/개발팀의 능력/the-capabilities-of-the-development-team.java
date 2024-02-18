928482c43ed6b5f2e4737c4c4be24f63f210185c
154448 <116735631+154448@users.noreply.github.com>
2024-02-18 15:23:27 +0900

    Title: 개발팀의 능력, Time: 135ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240218/개발팀의 능력/the-capabilities-of-the-development-team.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+
+public class Main {
+    public static boolean isDuplicate(int[] arr){
+        int n = arr.length;
+
+        boolean res = false;
+
+        for(int i = 0; i < n - 1; i++){
+            for(int j = i + 1; j < n; j++){
+                if(arr[i] == arr[j]){
+                    res = true;
+                    break;
+                } 
+            }
+        }
+
+        return res;
+
+    }
+
+    public static int findDiff(int[] arr){
+        int max = 0;
+        int min = 1000;
+
+        for(int i = 0; i < arr.length; i++){
+            max = Math.max(max, arr[i]);
+            min = Math.min(min, arr[i]);
+        }
+
+        return(max - min);
+    }
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner userIn = new Scanner(System.in);
+
+        int[] ability = new int[5];
+        int tot = 0;
+
+        int diff = 5000;
+
+        for(int i = 0; i < 5; i++){
+            int now = userIn.nextInt();
+            ability[i] = now;
+            tot += now;
+        }
+
+
+        //s : 한 명인 팀
+        for(int s = 0; s < 5; s++){
+            for(int i = 0; i < 4; i++){
+                if(i == s){
+                    continue;
+                }
+                else{
+                    for(int j = i + 1; j < 5; j++){
+                        if(j == s){
+                            continue;
+                        }
+                        else{
+                            int team = ability[i] + ability[j];
+                            int[] teamAbility = {s, team, tot - s - team};
+
+                            if(isDuplicate(teamAbility)){
+                                continue;
+                            }
+                            else{
+                                int currentDiff = findDiff(teamAbility);
+                                diff = Math.min(diff, currentDiff);
+                            }
+                        }
+                    }
+
+                }
+                
+            }
+
+        }
+
+        System.out.printf("%d", diff == 5000 ? -1 : diff);
+    }
+}
