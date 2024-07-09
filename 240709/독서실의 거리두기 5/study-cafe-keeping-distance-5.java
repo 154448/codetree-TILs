c032e6a72f8cfca1a8542cebe8d0365917e9e3b8
whfflaz <116735631+154448@users.noreply.github.com>
2024-07-09 14:39:10 +0900

    Title: 독서실의 거리두기 5, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240709/독서실의 거리두기 5/study-cafe-keeping-distance-5.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner userIn = new Scanner(System.in);
+
+        int n = userIn.nextInt();
+
+        char[] user = userIn.next().toCharArray();
+
+        int maxDist = 0;
+
+        for(int i = 0; i < n; i++){
+            if(user[i] == '1'){
+                continue;
+            }
+            else{
+                char[] tmp = user.clone();
+                tmp[i] = '1';
+
+                int minDist = 100;
+
+                boolean isExit = false;
+                int lastIdx = -1;
+
+                for(int j = 0; j < n; j++){
+                    if(tmp[i] == '1'){
+                        if(isExit){
+                            minDist = Math.min(minDist, j - lastIdx + 1);
+                            lastIdx = j;
+
+                        }
+                        else{
+                            isExit = true;
+                            lastIdx = j;
+                        }
+                    }
+                }
+
+                maxDist = Math.max(minDist, maxDist);
+            }
+        }
+
+        System.out.printf("%d", maxDist);
+
+
+    }
+}
