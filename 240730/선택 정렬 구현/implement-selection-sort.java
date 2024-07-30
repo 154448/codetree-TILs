8d8810aa6f697210a2ad0fbabef39a288ed51d2a
whfflaz <116735631+154448@users.noreply.github.com>
2024-07-30 21:56:41 +0900

    Title: 선택 정렬 구현, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240730/선택 정렬 구현/implement-selection-sort.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //Scanner
+        Scanner userIn = new Scanner(System.in);
+        //n : 전체 입력 받을 숫자의 수
+        int n = userIn.nextInt();
+        //nums : 입력받은 숫자를 저장하는 배열
+        int[] nums = new int[n];
+        //배열의 숫자들을 입력 받음
+        for(int i = 0; i < n; i++){
+            nums[i] = userIn.nextInt();
+        }
+
+        int minIdx = 0;
+        
+        //제일 작은 수부터 그 다음으로 작은 수를 차근 차근 찾음
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                if(nums[j] < nums[minIdx]){
+                    minIdx = j;
+                }
+            }
+
+            int tmp = nums[i];
+            nums[i] = nums[minIdx];
+            nums[minIdx] = tmp;
+
+            minIdx += 1;
+        }
+
+        for(int i = 0; i < n; i++){
+            System.out.printf("%d ", nums[i]);
+        }
+    }
+}
